80bd7cc1ed788c54a7e65ff8138d5e6eb9c848ae
Hristijan <chrispetrov904@gmail.com>
2023-03-11 17:36:37 +0100

    Added function for playing the game

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function getComputerChoice(){
 
 function play(player,computer1){
         
-        console.log(computer1);
+        
 
         let p=1
         switch(player.toLowerCase()){
@@ -27,9 +27,9 @@ function play(player,computer1){
             case "scissors": p=3
             break;
         }
-        console.log(p);
+        
         if(p==computer1){
-            return "It's a Tie."
+            return "It is a Tie."
         }
         else if(p==1 && computer1==2){
             return "You lose.Paper covers Rock!"
@@ -49,7 +49,25 @@ function play(player,computer1){
         else if(p==3 && computer1==2){
             return "You lose.Scissors cuts Paper!"
         }
-}  
-computer=getComputerChoice();
+} 
+function game(){
+    let Pscore=0;
+    let Cscore=0;
+    for(let i=0;i<5;i++){
+        let string1=play(prompt("Enter you Jajanken"),getComputerChoice());
+        console.log(string1.charAt(4));
+        if(string1.charAt(4)=="w"){
+            Pscore+=1;
+        }
+        else if(string1.charAt(4)=="l"){
+            Cscore+=1;
+        }
+        else console.log('no points are awarded :(');
+    }
+    if(Pscore>Cscore) console.log("PLAYER WINS");
+    else if(Pscore<Cscore) console.log("Computer wins");
+    else console.log("It's a TIE!?");
 
-console.log(play("paper",computer));
+}
+
+game();
